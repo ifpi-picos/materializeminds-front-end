061e481e2b1fd7c849e052ee1939e1fc215eeadc
samueloliveira_m <trabalho.samueloliv@gmail.com>
2023-11-21 20:49:01 -0300

    Implementation create supllier

--- js/createFornecedor.js
+++ js/createFornecedor.js
@@ -1,39 +1,47 @@
-export default async function enviarRequisicao() {
-    const url = 'https://api-materialize.onrender.com/suppliers'; 
+document.getElementById('form-create-supllier').addEventListener('submit', async (event) => {
+    event.preventDefault();
+    
     const dadosFornecedor = {
         nomeDaEmpresa: 'Nome da Empresa',
+        contato: '89981401967',
+        email: 'endereco@gmail.com',
+        senha: "Som2013",
         endereco: {
-            rua: 'Nome da Rua',
-            cidade: 'Nome da Cidade',
-            estado: 'Nome do Estado',
-            cep: 'Código Postal'
+            rua: 'Parinha',
+            cidade: 'picos',
+            estado: 'PI',
+            cep: '646405'
         },
-        contato: 'Nome do Contato',
-        email: 'endereco@email.com'
     };
+   
+    async function requestCreateSupllier(dadosFornecedor){
+         try {
+             const url = 'http://localhost:3333/supllier'; 
 
-    try {
-        const response =  await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(dadosFornecedor)
-        });
+            
+             const response =  await fetch(url, {
+                 method: 'POST',
+                 headers: {
+                     'Content-type': 'application/json'
+                 },
+                 body: JSON.stringify(dadosFornecedor)
+             });
+             
+             const dados = await response.json()
 
-        if( response.status === 201) {
-            alert("Papelaria cadastrada com sucesso!");
-            console.log('Fornecedor criado com sucesso!');
-            window.location.href = 'item.html';
-        } else if (response.status === 400) {
-            console.error('Requisição inválida.');
-        } else {
-            console.error('Erro desconhecido.');
-        }
-    } catch (erro) {
-        console.error('Erro na requisição:', erro.message);
+             if( response.status === 201) {
+                 alert("Papelaria cadastrada com sucesso!");
+                 window.location.href = 'item.html';
 
+             } else if (response.status === 400) {
+                if(dados.message==="Campos invalidos" || dados.message==="Endereço invalido")
+                alert(dados.message)
+             } else {
+                 console.error(`Erro desconhecido. \n ${dados.message}`);
+             }
+         } catch (erro) {
+             console.error('Erro na requisição:', erro.message);
+         }
     }
-
- enviarRequisicao();
-}
+    requestCreateSupllier(dadosFornecedor)
+})
--- js/createProduto.js
+++ js/createProduto.js
@@ -2,9 +2,15 @@ export async function criarProduto (dadosProduto) {
     const url = ' https://api-materialize.onrender.com/create-product';
 
     const formData = new FormData ();
+    
+    console.log(typeof(formData))
+
     Object.entries(dadosProduto).forEach(([chave, valor]) => {
         formData.append(chave, valor);
     });
+    console.log(typeof(formData))
+    console.log('|')
+    console.log(formData)
     
     try {
         const response = await fetch(url, {
