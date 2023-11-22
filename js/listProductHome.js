d560a1ea3aec8aeb57cb67eadb41989b5f8830fc
samueloliveira_m <trabalho.samueloliv@gmail.com>
2023-11-22 10:58:25 -0300

    Implementation display product home

--- js/listProductHome.js
+++ js/listProductHome.js
@@ -1,13 +1,60 @@
-function adicionarConteudoDinamico() {
-	// Obtém o elemento onde o conteúdo será adicionado
-	var elemento = document.getElementById("conteudo-dinamico");
 
-	// Cria um novo elemento de parágrafo
-	var paragrafo = document.createElement("p");
+async function getRequestProduct() {
+	const url = ' https://api-materialize.onrender.com/product';
 
-	// Adiciona texto ao parágrafo
-	paragrafo.textContent = "Este é um conteúdo dinâmico adicionado com JavaScript.";
-
-	// Adiciona o parágrafo ao elemento principal
-	elemento.appendChild(paragrafo);
+	try {
+		const response = await fetch(url, {
+			method: 'GET', 
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+	
+		if (response.status == 200) {
+			const dataProduct = await response.json();
+			console.log("DFD")
+			criarCards(dataProduct)
+		} else {
+			console.error('Erro na requisição:', response.status);
+		}
+	} catch (error) {
+		console.error('Erro ao realizar a requisição:', error);
+	}
 }
+
+function criarCards(data) {
+  const container = document.getElementById('product-list');
+
+  container.innerHTML = '';
+
+  data.forEach(item => {
+    const cardDiv = document.createElement('div');
+    cardDiv.className = 'card';
+
+    const imagem = document.createElement('img');
+    imagem.src = item.imageUrl; 
+    imagem.alt = '';
+
+    const nomeProduto = document.createElement('p');
+    nomeProduto.className = 'name-produt';
+    nomeProduto.textContent = item.nomeDoProduto; 
+    const preco = document.createElement('span');
+    preco.className = 'price';
+		const oldPrice = item.preco+20
+    preco.innerHTML = `R$ ${item.preco} <del>R$ ${oldPrice}</del>`; 
+
+    const linkCompra = document.createElement('a');
+    linkCompra.className = 'button';
+    linkCompra.href = item.link; 
+    linkCompra.textContent = 'Compra';
+
+    cardDiv.appendChild(imagem);
+    cardDiv.appendChild(nomeProduto);
+    cardDiv.appendChild(preco);
+    cardDiv.appendChild(linkCompra);
+
+    container.appendChild(cardDiv);
+  });
+}
+
+getRequestProduct()
