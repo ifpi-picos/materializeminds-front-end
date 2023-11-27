430bd4293c968695fbb7f07cd8d7fbdcc2493eaf
karolyne04 <ferreiracarolyne04@gmail.com>
2023-11-27 18:32:31 -0300

    feat: responsivo

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-import { enviarRequisicao } from './createFornecedor';
-import { criarProduto } from './createProduto';
-
-
-// Chame a função para enviar a requisição quando necessário
-enviarRequisicao();
-
-const dadosProduto = {
-    nomeDoProduto: 'Nome do produto',
-    descricao: 'Descrição do produto',
-    preco: 29.99,
-    estoque: 100,
-    categoria: 'lapis',
-    supplierId: 'ID_DO_FORNECEDOR'
-};
- criarProduto(dadosProduto);
+document.getElementById('mobile-menu').addEventListener('click', function() {
+    document.getElementById('nav-list').classList.toggle('show');
+  });
