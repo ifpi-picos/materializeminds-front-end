f8205ea2b0077a6ad877e22814c0890c0906facb
samueloliveira_m <trabalho.samueloliv@gmail.com>
2023-11-29 13:34:08 -0300

    implementação de html para cadastrar produto

--- js/createProduto.js
+++ js/createProduto.js
@@ -1,58 +1,41 @@
-//document.getElementById('form-papelaria-item').addEventListener('submit', async (event) => {
-  //  event.preventDefault();
+document.getElementById('form-papelaria-item').addEventListener('submit', async (event) => {
+    event.preventDefault();
   
- async function criarProduto (dadosProduto, idFornecedor) {
-    const url = ' https://api-materialize.onrender.com/create-product';
- 
 
-    const imagemPro = document.getElementById('imagem-produto').value;
-    const descricaoPro = document.getElementById('descricao-produto').value;
-    const categoriaPro = document.getElementById('categoria-produto').value;
-    const valorPro = document.getElementById('valor-produto').value;
-    const quantidadePro = document.getElementById('Quantidade-produto').value;
+    var formData = new FormData(this);
 
-    const dadosProduto = {
-        imagemPro,
-        descricaoPro,
-        categoriaPro,
-        valorPro,
-        quantidadePro
-    }
-    const idFornecedor = "id-for";
-    //Adiciona o ID do fornecedor aos dados do produto
-    dadosProduto.supplierId = idFornecedor;
-    const formData = new FormData ();
-    const dadosProduto = {
+    var produtoInfo = {
+        nomeDoProduto: document.getElementById('nomeProduto').value,
+        descricao: document.getElementById('nomeProduto').value,
+        preco: document.getElementById('nomeProduto').value,
+        estoque: document.getElementById('nomeProduto').value,
+        categoria: document.getElementById('nomeProduto').value,
+        supplierId: document.getElementById('nomeProduto').value,
+    };
 
-    }
+    formData.append('produtoInfo', JSON.stringify(produtoInfo));
+
+
+})
     
-    console.log(typeof(formData))
-
-    Object.entries(dadosProduto).forEach(([chave, valor]) => {
-        formData.append(chave, valor);
+try {
+    const response = await fetch(url, {
+        method : 'POST',
+        body : formData
     });
-    console.log(typeof(formData))
-    console.log('|')
-    console.log(formData)
-    
-    try {
-        const response = await fetch(url, {
-            method : 'POST',
-            body : formData
-        });
 
-        if (response.status === 201) {
-            alert("Produto criado com sucesso!");
-            console.log('produto criado com sucesso!');
-        } else if (response.status === 400) {
-            console.error('Requisição inválida.');
-        } else {
-            console.error('Erro desconhecido.');
-        }
-    } catch (error) {
-        console.error('Erro na requisição:', error.message);
+    if (response.status === 201) {
+        alert("Produto criado com sucesso!");
+        console.log('produto criado com sucesso!');
+    } else if (response.status === 400) {
+        console.error('Requisição inválida.');
+    } else {
+        console.error('Erro desconhecido.');
     }
+} catch (error) {
+    console.error('Erro na requisição:', error.message);
+}
 
- criarProduto(dadosProduto, idFornecedor);
-};
+criarProduto(dadosProduto, idFornecedor);
+
 
